622dbb7446fd6c3a4b0c52cfb7a45566285bd67e
Gabriel Gregerson <300097216@fvtc.edu>
2024-10-04 22:08:42 -0500

    Added carousal

--- site.js
+++ site.js
@@ -36,3 +36,46 @@ else if (isEvening) {
     WelcomeDiv.innerHTML = "Good Evening!"
 }
 localStorage.setItem("It's a secret to everybody.", "It's dangerous to go alone! Take this." )
+
+
+
+const urls = [
+    'https://images.pexels.com/photos/1454360/pexels-photo-1454360.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/933964/pexels-photo-933964.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/267885/pexels-photo-267885.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/1251861/pexels-photo-1251861.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/1370296/pexels-photo-1370296.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+].map(url => { (new Image()).src = url; return url })
+
+const images = document.querySelectorAll('#carousel img')
+
+let currentImage = 0
+const showImages = () => {
+    const offset = currentImage % urls.length
+    images.forEach((image, index) => {
+        const imageIndex = (index + offset + urls.length) % urls.length
+        image.src = urls[imageIndex]
+    })
+}
+
+showImages()
+
+const nextImage = () => {
+    currentImage++;
+    showImages();
+};
+
+const prevImage = () => {
+    currentImage--;
+    if (currentImage < 0) {
+        currentImage = urls.length - 1;
+    }
+    showImages();
+};
+
+document.getElementById('next').addEventListener('click', () => nextImage());
+document.getElementById('prev').addEventListener('click', () => prevImage());
+
+setInterval(() => {
+    nextImage();
+}, 5000);
